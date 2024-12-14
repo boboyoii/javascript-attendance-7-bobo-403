3b4993353d6c471d4968f1bfa6d0cd4282daecf8
bobo-403 <139124147+bobo-403@users.noreply.github.com>
2024-12-14 17:39:37 +0900

    feat: 해당 닉네임 출석 기록 보는 기능 추가

--- src/App.js
+++ src/App.js
@@ -1,22 +1,28 @@
 import { FEATURE_SELECTION } from './constant/Message.js';
+import Manager from './controller/Manager.js';
 import retry from './retry.js';
 import UserInput from './view/UserInput.js';
 
 class App {
   #userInput;
+  #manager;
 
   constructor() {
     this.#userInput = new UserInput();
+    this.#manager = new Manager();
   }
 
   async run() {
     let reStart = true;
     do {
       const featureNum = await retry(() => this.#userInput.inputFeatureNum());
-      if (featureNum === FEATURE_SELECTION[0]) console.log('출석확인');
-      if (featureNum === FEATURE_SELECTION[1]) console.log('출석수정');
-      if (featureNum === FEATURE_SELECTION[2]) console.log('출석수정');
-      if (featureNum === FEATURE_SELECTION[3]) console.log('출석수정');
+
+      if (featureNum === FEATURE_SELECTION[0]) await this.#manager.attendance();
+      if (featureNum === FEATURE_SELECTION[1])
+        await this.#manager.editAttendance();
+      if (featureNum === FEATURE_SELECTION[2])
+        await this.#manager.attendanceRecord();
+      if (featureNum === FEATURE_SELECTION[3]) console.log('제직 위험자 확인');
       if (featureNum === FEATURE_SELECTION[4]) reStart = false;
     } while (reStart);
   }
