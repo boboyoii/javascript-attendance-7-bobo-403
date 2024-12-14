c926e7b8a42aded2c8a5d567eb7798ca2486c27d
bobo-403 <139124147+bobo-403@users.noreply.github.com>
2024-12-14 14:44:42 +0900

    feat: 주말 또는 공휴일인 아닌지 확인하는 기능 추가

--- src/controller/Validator.js
+++ src/controller/Validator.js
@@ -1,9 +1,20 @@
-import { ERROR_MESSAGE, FEATURE_SELECTION } from '../constant/Message';
+import {
+  DAY,
+  ERROR_MESSAGE,
+  FEATURE_SELECTION,
+  HOLIDAY_DATE,
+} from '../constant/Message';
 
 class Validator {
   validateFeatureSelect(select) {
     if (!FEATURE_SELECTION.includes(select)) {
       throw new Error(ERROR_MESSAGE.invalid__format);
+    }
+  }
+
+  validateHoliday(date) {
+    if (HOLIDAY_DATE.includes(date)) {
+      throw new Error(ERROR_MESSAGE.invalid_date(date, DAY[date % 7]));
     }
   }
 }
