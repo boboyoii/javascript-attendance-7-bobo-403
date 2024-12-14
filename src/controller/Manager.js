6a418574e0e392cef9efd1e8237b4fb3c0cf97a7
bobo-403 <139124147+bobo-403@users.noreply.github.com>
2024-12-14 16:12:09 +0900

    feat: 출석 확인 기능 추가

---
+++ src/controller/Manager.js
@@ -0,0 +1,36 @@
+import { Console, DateTimes } from '@woowacourse/mission-utils';
+import Roll from '../model/Roll.js';
+import retry from '../retry.js';
+import UserInput from '../view/UserInput.js';
+import { DAY } from '../constant/Message.js';
+import Validator from './Validator.js';
+
+class Manager {
+  #roll;
+  #userInput;
+  #validator;
+
+  constructor() {
+    this.#roll = new Roll();
+    this.#userInput = new UserInput();
+    this.#validator = new Validator();
+  }
+
+  async attendance() {
+    try {
+      const today = DateTimes.now();
+      const month = today.getMonth() + 1;
+      const date = today.getDate();
+      this.#validator.validateHoliday(date);
+      const day = DAY[date % 7];
+      const name = await this.#userInput.inputName();
+      const time = await retry(() => this.#userInput.inputTime());
+      const state = this.#roll.setState(day, time);
+      Console.print(`\n${month}월 ${date}일 ${day}요일 ${name} ${state}\n`);
+    } catch (error) {
+      Console.print(error.message + '\n');
+    }
+  }
+}
+
+export default Manager;
