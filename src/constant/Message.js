44f6caa76a08cbef52c7306454eaf4ea653e39cd
bobo-403 <139124147+bobo-403@users.noreply.github.com>
2024-12-14 14:51:17 +0900

    feat: 진행 메시지 추가

--- src/constant/Message.js
+++ src/constant/Message.js
@@ -21,3 +21,19 @@ export const DAY = Object.freeze({
 });
 
 export const FEATURE_SELECTION = ['1', '2', '3', '4', 'Q'];
+
+export const PROGRESS_MESSAGE = Object.freeze({
+  enter_feature_num: (
+    date,
+    day
+  ) => `오늘은 12월 ${date}일 ${day}입니다. 기능을 선택해 주세요.\n 1. 출석 확인\n
+2. 출석 수정\n
+3. 크루별 출석 기록 확인\n
+4. 제적 위험자 확인\n
+Q. 종료\n`,
+  enter_name: '닉네임을 입력해 주세요.\n',
+  enter_time: '등교 시간을 입력해 주세요.\n',
+  enter_edit_name: '출석을 수정하려는 크루의 닉네임을 입력해 주세요.\n',
+  enter_edit_date: '수정하려는 날짜(일)를 입력해 주세요.\n',
+  enter_edit_time: '언제로 변경하겠습니까?\n',
+});
