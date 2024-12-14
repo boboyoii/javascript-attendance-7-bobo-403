03d3edff299129990ca877f37de785dfa35c9b2e
bobo-403 <139124147+bobo-403@users.noreply.github.com>
2024-12-14 15:17:53 +0900

    fix: 매개 변수 전달 추가

--- src/view/UserInput.js
+++ src/view/UserInput.js
@@ -1,4 +1,4 @@
-import { Console } from '@woowacourse/mission-utils';
+import { Console, DateTimes } from '@woowacourse/mission-utils';
 import { PROGRESS_MESSAGE } from '../constant/Message.js';
 import Validator from '../controller/Validator.js';
 
@@ -8,8 +8,11 @@ class UserInput {
     this.#validator = new Validator();
   }
   async inputFeatureNum() {
+    const today = DateTimes.now();
+    const month = today.getMonth() + 1;
+    const date = today.getDate();
     const featureNum = await Console.readLineAsync(
-      PROGRESS_MESSAGE.enter_feature_num
+      PROGRESS_MESSAGE.enter_feature_num(month, date)
     );
     this.#validator.validateFeatureSelect(featureNum);
     return featureNum;
