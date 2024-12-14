b61f5f5d00a53a77165c89e4b16cde6d6899be0f
bobo-403 <139124147+bobo-403@users.noreply.github.com>
2024-12-14 15:18:19 +0900

    feat: 초기 메인 로직 함수 추가

--- src/App.js
+++ src/App.js
@@ -1,5 +1,25 @@
+import { FEATURE_SELECTION } from './constant/Message.js';
+import retry from './retry.js';
+import UserInput from './view/UserInput.js';
+
 class App {
-  async run() {}
+  #userInput;
+
+  constructor() {
+    this.#userInput = new UserInput();
+  }
+
+  async run() {
+    let reStart = true;
+    do {
+      const featureNum = await retry(() => this.#userInput.inputFeatureNum());
+      if (featureNum === FEATURE_SELECTION[0]) console.log('출석확인');
+      if (featureNum === FEATURE_SELECTION[1]) console.log('출석수정');
+      if (featureNum === FEATURE_SELECTION[2]) console.log('출석수정');
+      if (featureNum === FEATURE_SELECTION[3]) console.log('출석수정');
+      if (featureNum === FEATURE_SELECTION[4]) reStart = false;
+    } while (reStart);
+  }
 }
 
 export default App;
